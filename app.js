ad1f1fef8f31b614f163f2cde50b9b2de1770059
Cüneyt Kaya <53535761+kayacuneyd@users.noreply.github.com>
2024-03-10 21:52:34 +0000

    icons added

--- app.js
+++ app.js
@@ -53,7 +53,13 @@ async function searchCountry() {
                     <div class="lc-block mb-3">
                           <div editable="rich">
 
-                              <h2 class="h5">${country.country}</h2>
+                              <h2 class="h5">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check2-circle" viewBox="0 0 16 16">
+                                    <path d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0"/>
+                                    <path d="M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0z"/>
+                                </svg>
+                              ${country.country}
+                              </h2>
 
                               <p>You can apply for e-visa to travel to Turkey.</p>
                           </div>

73051568ca8edc518502b512f62be9820f9046aa
Cüneyt Kaya <53535761+kayacuneyd@users.noreply.github.com>
2024-03-14 09:24:53 +0000

    some ui fixes on select area

--- app.js
+++ app.js
@@ -4,15 +4,22 @@ $(document).ready(function() {
         ajax: {
             url: 'https://gist.githubusercontent.com/kayacuneyd/08f9d3656cbccf9634d2ee2894be5e1f/raw/cc0699c00485eb31969728715f859224c36ad01d/data.json',
             dataType: 'json',
+            delay: 250, // Klavye girişlerinden sonra istek yapmadan önce bekleyecek süre (ms)
             processResults: function (data) {
                 return {
-                    results: data.countries.map(country => ({
-                        id: country.country,
-                        text: country.country,
+                    results: data.countries.map(item => ({
+                        id: item.code, // Her ülkenin benzersiz id'si, burada ülke kodu kullanıldı
+                        text: item.country // Gösterilecek metin
                     }))
                 };
-            }
+            },
+            cache: true
         }
+    });
+
+    // Document hazır olduğunda Select2'yi başlat
+    $(document).ready(function() {
+        initializeSelect2();
     });
 
     $('#countrySelect').on('select2:select', async function (e) {

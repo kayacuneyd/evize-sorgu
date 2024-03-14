58e0d5ca4f4d78fbbdb030279f36a133683f654f
Cüneyt Kaya <53535761+kayacuneyd@users.noreply.github.com>
2024-03-14 09:39:13 +0000

    ui fixes vol.18

--- app.js
+++ app.js
@@ -1,33 +1,21 @@
-$(document).ready(function() {
-    $('#countrySelect').select2({
-        placeholder: 'Ülke seçin',
-        ajax: {
-            url: 'https://gist.githubusercontent.com/kayacuneyd/08f9d3656cbccf9634d2ee2894be5e1f/raw/cc0699c00485eb31969728715f859224c36ad01d/data.json',
-            dataType: 'json',
-            delay: 250, // Klavye girişlerinden sonra istek yapmadan önce bekleyecek süre (ms)
-            processResults: function (data) {
-                return {
-                    results: data.countries.map(country => ({
-                        id: country.code,
-                        text: country.country,
-                    }))
-                };
-            },
-            cache: true
-        }
-    });
+$(document).ready(async function() {
+    const countries = await loadCountries();
+    if (countries) {
+        $('#countrySelect').select2({
+            width: '100%',
+            placeholder: 'Ülke seçin',
+            data: countries.map(country => ({
+                id: country.code, // Select2 içinde birbirinden ayırt etmek için kullanılır
+                text: country.country // Kullanıcıya gösterilecek metin
+            }))
+        });
+    }
 
-    // Document hazır olduğunda Select2'yi başlat
-    $(document).ready(function() {
-        initializeSelect2();
-    });
-
-    $('#countrySelect').on('select2:select', async function (e) {
-        const selectedCountryName = e.params.data.id;
-        const countries = await loadCountries();
-        const country = countries.find(c => c.country === selectedCountryName);
+    $('#countrySelect').on('select2:select', function (e) {
+        const selectedCountryCode = e.params.data.id;
+        const country = countries.find(c => c.code === selectedCountryCode);
         if (country) {
-            document.getElementById('result').innerHTML = `
+            $('#result').html(`
                 <div class="card mx-auto">
                     <div class="card-body">
                         <img class="img-fluid" width="512" src="${country.flag_url}" alt="Flag of ${country.country}">
@@ -36,21 +24,7 @@ $(document).ready(function() {
                         <a class="btn btn-primary" target="_blank" href="https://evisa.gov.tr/en/apply/" role="button">Go and Apply</a>
                     </div>
                 </div>
-            `;
+            `);
         }
     });
 });
-
-async function loadCountries() {
-    try {
-        const response = await fetch('https://gist.githubusercontent.com/kayacuneyd/08f9d3656cbccf9634d2ee2894be5e1f/raw/cc0699c00485eb31969728715f859224c36ad01d/data.json');
-        if (!response.ok) {
-            throw new Error('Veri yüklenemedi: ' + response.statusText);
-        }
-        const data = await response.json();
-        return data.countries;
-    } catch (error) {
-        console.error('Veri yükleme hatası:', error);
-        return null;
-    }
-}
